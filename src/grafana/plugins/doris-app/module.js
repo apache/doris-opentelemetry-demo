2b2100066c6446f8f335ae0bca0379f375a307e0
Jeffrey <color.dove@gmail.com>
2025-08-18 19:33:42 +0800

    fix: generateUid works on http protocol

--- src/grafana/plugins/doris-app/module.js
+++ src/grafana/plugins/doris-app/module.js
@@ -1192,7 +1192,7 @@ module.exports = domAPI;
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "" + chunkId + ".js?_cache=" + {"35":"6d86e876a981760ae4a1","181":"ce826c664576171bad6c","202":"960b652c09389756ed7b","462":"5440070870454b81ce34","632":"20f9d65c2526bb592e0a","723":"cd85aac21ae242f5f0f2","892":"24dff9e638b25a5f1233","906":"c5c71c22310462d0fa64"}[chunkId] + "";
+/******/ 			return "" + chunkId + ".js?_cache=" + {"35":"6d86e876a981760ae4a1","181":"ce826c664576171bad6c","202":"960b652c09389756ed7b","462":"5440070870454b81ce34","632":"20f9d65c2526bb592e0a","723":"cd85aac21ae242f5f0f2","892":"82e6050ef02993d795d5","906":"c5c71c22310462d0fa64"}[chunkId] + "";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -1282,7 +1282,7 @@ module.exports = domAPI;
 /******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
-/******/ 	__webpack_require__.sriHashes = {"35":"sha256-8TWj0IuqCnw+QkYOR4eAnmDEKCIKywuChqXPt3S4FV0=","181":"sha256-jQioVDwy6+zI6R5tkOP1uFevBjj+G6LCx15ntqcO/8M=","202":"sha256-CN4kzTw1qZgwAinJ1/Yabb8xvpGp0KP+/BizIwiPgfQ=","462":"sha256-JS4wDqS1h7LXA99a07Iy4ej0It/6EsktYnxY1AhffHw=","632":"sha256-MW1oi1N0a/3ek7LCgIZcCVt3rVRNjLXJmSZD8oTGIZY=","723":"sha256-YDvp7Qjz8uu5Sk0fPOr2QrUrKd0OfhzUsNXFLgjBQwM=","892":"sha256-NYdhd6aUU1lpWGlumdC48ds3z8Fa3UBeQ0eFx0qOXS0=","906":"sha256-tEhWWkd4HOGiN1BQ29N7h1ly0ZIhLQ+U7xB5yd1YusU="};
+/******/ 	__webpack_require__.sriHashes = {"35":"sha256-8TWj0IuqCnw+QkYOR4eAnmDEKCIKywuChqXPt3S4FV0=","181":"sha256-jQioVDwy6+zI6R5tkOP1uFevBjj+G6LCx15ntqcO/8M=","202":"sha256-CN4kzTw1qZgwAinJ1/Yabb8xvpGp0KP+/BizIwiPgfQ=","462":"sha256-JS4wDqS1h7LXA99a07Iy4ej0It/6EsktYnxY1AhffHw=","632":"sha256-MW1oi1N0a/3ek7LCgIZcCVt3rVRNjLXJmSZD8oTGIZY=","723":"sha256-YDvp7Qjz8uu5Sk0fPOr2QrUrKd0OfhzUsNXFLgjBQwM=","892":"sha256-XiOYVIZWnYRcLAJlncmg4EGCFmZyjy+5CalJVSwaats=","906":"sha256-tEhWWkd4HOGiN1BQ29N7h1ly0ZIhLQ+U7xB5yd1YusU="};
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
 /******/ 	(() => {
